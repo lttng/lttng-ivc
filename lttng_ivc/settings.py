dc3b014c2706cfc7d5ed64e19e197303520acf4d
Jonathan Rajotte <jonathan.rajotte-julien@efficios.com>
2017-10-26 11:13:38 -0400

    Add preload_provider app
    Shared object provider can be built and used independently of the app under
    test.
    
    Signed-off-by: Jonathan Rajotte <jonathan.rajotte-julien@efficios.com>

--- lttng_ivc/settings.py
+++ lttng_ivc/settings.py
@@ -11,6 +11,7 @@ git_remote_folder = os.path.dirname(os.path.abspath(__file__)) + "/runtime/git_r
 app_folder = os.path.dirname(os.path.abspath(__file__)) + "/runtime/git_remote"
 apps_folder = os.path.dirname(os.path.abspath(__file__)) + "/apps"
 apps_gen_events_folder = os.path.join(apps_folder, "gen_ust_events")
+apps_preload_provider_folder = os.path.join(apps_folder, "preload_provider")
 
 tmp_object_prefix = "lttng-ivc-"
 
