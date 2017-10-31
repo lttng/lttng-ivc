f858da7bf598054532a3ad16955eb4956c576fa6
Jonathan Rajotte <jonathan.rajotte-julien@efficios.com>
2017-10-31 15:36:30 -0400

    Rename to must_be_root
    Signed-off-by: Jonathan Rajotte <jonathan.rajotte-julien@efficios.com>

--- lttng_ivc/utils/skip.py
+++ lttng_ivc/utils/skip.py
@@ -1,4 +1,4 @@
 import os
 import pytest
 
-root = pytest.mark.skipif(os.geteuid() != 0, reason="Must be run as root")
+must_be_root = pytest.mark.skipif(os.geteuid() != 0, reason="Must be run as root")
