35d51bdc101925259ac60d6ea9fe5abd9e5a381b
Jonathan Rajotte <jonathan.rajotte-julien@efficios.com>
2019-06-03 11:45:24 -0400

    Fix: line_count variable external obscuration
    Signed-off-by: Jonathan Rajotte <jonathan.rajotte-julien@efficios.com>

--- lttng_ivc/utils/utils.py
+++ lttng_ivc/utils/utils.py
@@ -29,11 +29,11 @@ from contextlib import closing
 
 
 def line_count(file_path):
-    line_count = 0
+    count = 0
     with open(file_path) as f:
-        for line in f:
-            line_count += 1
-    return line_count
+        for i in f:
+            count += 1
+    return count
 
 
 def sha256_checksum(filename, block_size=65536):
